8cb0081ec4548f688eca7eb423e21bacf5383978
NataliaValchuk <trewax321@gmail.com>
2023-03-22 19:23:04 +0200

    added gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+let instance;
+
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+  </li>
+`;
+
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const imageSrc = event.target.dataset.source;
+
+  if (imageSrc) {
+    instance = basicLightbox.create(`
+      <img src="${imageSrc}">
+    `);
+
+    instance.show();
+  }
+});
+
+const renderGallery = () => {
+  const galleryMarkup = galleryItems
+    .map((item) => createGalleryItem(item))
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+};
+
+const openModal = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  document
+    .querySelector('.modal__button')
+    .addEventListener('click', closeModal);
+};
+
+const closeModal = () => {
+  instance.close();
+};
+
+renderGallery();
